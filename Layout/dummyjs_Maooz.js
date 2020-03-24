3137f7084af02efa37c84558bb79d284c075c641
Maooz Bin Zahid <mbzahid.bese17seecs@seecs.edu.pk>
2020-03-24 07:29:02 +0500

    Column 1 developed

---
+++ Layout/dummyjs_Maooz.js
@@ -0,0 +1,66 @@
+//global response variable
+
+var JSONResponse;
+
+
+$(document).ready(function() {
+    //this will be the response of the API
+	JSONResponse = [{
+					"name" : "Mom's Spaghetti",
+					"description" : "There's vomit on his sweater already",
+					"img":"moms_spaghetti.jpg"},
+					{"name" : "Mom's Spaghetti 2",
+					"description" : "There's vomit on his sweater already",
+					"img":"moms_spaghetti.jpg"},
+					{"name" : "Mom's Spaghetti 3",
+					"description" : "There's vomit on his sweater already",
+					"img":"moms_spaghetti.jpg"},
+					{
+					"name" : "Mom's Spaghetti 4",
+					"description" : "There's vomit on his sweater already",
+					"img":"moms_spaghetti.jpg"},
+					{"name" : "Mom's Spaghetti 5",
+					"description" : "There's vomit on his sweater already",
+					"img":"moms_spaghetti.jpg"},
+					{"name" : "Mom's Spaghetti 6",
+					"description" : "There's vomit on his sweater already",
+					"img":"moms_spaghetti.jpg"},
+					{
+					"name" : "Mom's Spaghetti 7",
+					"description" : "There's vomit on his sweater already",
+					"img":"moms_spaghetti.jpg"},
+					{"name" : "Mom's Spaghetti 8",
+					"description" : "There's vomit on his sweater already",
+					"img":"moms_spaghetti.jpg"},
+					{"name" : "Mom's Spaghetti 9",
+					"description" : "There's vomit on his sweater already",
+					"img":"moms_spaghetti.jpg"}
+					];
+				
+
+var html = ""
+for(var i=0 ; i< JSONResponse.length ; i++){
+	html = html + `<div class="row"><img class="recipe_image" src=${JSONResponse[i].img}><div><p class="recipe_name"><a class="rec" href="#">${JSONResponse[i].name}</a></p><p class="recipe_description">${JSONResponse[i].description}</p></div></div>`;
+}
+
+document.getElementById("first_column").innerHTML = html;
+
+//adding on click method of each of list items displayed
+$(".rec").on("click",function(){
+	console.log(onRecipeNameClick($(this).text()));
+});
+
+    
+});
+
+//function that returns the index of object clicked
+function onRecipeNameClick(val){
+	var index = -1;
+	JSONResponse.find(function(item, i){
+  		if(item.name === val){
+  		index = i;	
+    	return i;
+  		}
+	});
+	return index;
+}
