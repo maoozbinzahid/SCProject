2981b0379c1d8b2fbec4b865814a9e413ba6cf31
HasnatKhan <34575396+HasnatKhan@users.noreply.github.com>
2020-04-03 17:05:52 +0500

    Update controller.js

--- Layout/controller.js
+++ Layout/controller.js
@@ -125,17 +125,15 @@ function PopulateColumnOne(JSONResponse){
 
 	//adding on click method of each of list items displayed
 	$(".rec").on("click",function(){
-<<<<<<< HEAD
+
 		//console.log(onRecipeNameClick($(this).text(), JSONResponse));
 		response = onRecipeNameClick($(this).text(), JSONResponse);
 		htmlc2 = `<div class="card"><div class="header"><img  src="${JSONResponse[response].image_url}" alt = "Picture for recipe" id = "rimage"  style="background-size: cover"><div class="icon"><a href="#"><i class=" fa fa-heart"></i></a></div></div><div class="text"><h1 class="food" id = "rname">${JSONResponse[response].title}</h1><i class="fa fa-clock" aria-hidden="true"> 15 Mins</i><i class="fa fa-users"> Serves 2</i><div class="stars"><li><a href="#"><i class="fa fa-star"></i></a><a href="#"><i class="fa fa-star"></i></a><a href="#"><i class="fa fa-star"></i></a><a href="#"><i class="fa fa-star"></i></a><a href="#"><i class="fa fa-star-o"></i></a></li></div><p class="info" id = ""rinfo> ">${JSONResponse[response].publisher}</p></div><a href="#" class="btn1">Let's Cook!</a></div>`;
 		recipe_column.innerHTML = htmlc2;
-		
-		 
-=======
+
 		selectedItem_Index = onRecipeNameClick($(this).text(), JSONResponse);
 		console.log("Selected Item is:" + selectedItem_Index);
->>>>>>> parent of 18e95cf... Merge branch 'Hasnat'
+
 	});
 
 	//enables like button
