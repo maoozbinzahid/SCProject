abecd112e38532ed0361ebde7fd1ac88936d69ca
saadakram-372 <52201803+saadakram-372@users.noreply.github.com>
2020-04-08 21:33:47 +0500

    pagination

--- Layout/SearchView.js
+++ Layout/SearchView.js
@@ -2,3 +2,10 @@
 export var searchButton = document.getElementById('search_btn');
 export var searchBox = document.getElementById("searchbox");
 export var output = document.getElementById("second_column");
+<<<<<<< Updated upstream
+=======
+
+//added by Maooz
+
+export var column1 = document.getElementById('1st_column');
+>>>>>>> Stashed changes
