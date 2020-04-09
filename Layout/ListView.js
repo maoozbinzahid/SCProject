5827f41ae0a2cf64c5e9cd01140756cbc59f3dc6
Maooz Bin Zahid <mbzahid.bese17seecs@seecs.edu.pk>
2020-04-09 07:42:56 +0500

    Revert "Merge remote-tracking branch 'origin/Mahnoor'"
    This reverts commit 99ad4c19d365bba7052996cccbfaeddf48c7fc13, reversing
    changes made to bbf73f2566263e014cd50facd6e580d150e716d1.

--- Layout/ListView.js
+++
@@ -1,4 +0,0 @@
-export var checkoutButton = document.getElementById('check-out-btn');
-export var i=document.getElementsByClassName("qty");
-export var ingredients=document.getElementById("Ingredients");
-export var def_serving= (document.getElementsByClassName("fa-users"))[0];
